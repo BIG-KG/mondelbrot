66a2194b08de2d36597eab56aa3e88f83038358c
GeorgeBek <bek.georogy@gmail.com>
2025-04-03 18:57:33 +0300

    add SDL

--- mondelbrot_Base.cpp
+++ mondelbrot_Base.cpp
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include <SDL2/SDL.h>
 
 
+const int x_size = 800;
+const int y_size = 600;
 const int repeats_to_set = 50;
+const int num_of_frames = 1000;
 const double max_quadr_Range = 100.0;
+
 
 
 int calculate_pixel_color(int x, int y, int x_center, int y_center, double zoom) 
@@ -26,9 +31,9 @@ int calculate_pixel_color(int x, int y, int x_center, int y_center, double zoom)
         repeats++;
     }
 
-    if (repeats >= repeats_to_set)
+    if (r < max_quadr_Range)
     {
-        return repeats;
+        return 1;
     }
 
     return 0;        
@@ -36,20 +41,60 @@ int calculate_pixel_color(int x, int y, int x_center, int y_center, double zoom)
 
 int main()
 {
-    int x_size = 800;
-    int y_size = 600;
     int x_center = 0.0;
     int y_center = 0.0;
     double zoom = 20.0;
 
-    for (int x = 0; x < x_size; x++)
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Error SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Mondelbrot array",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        x_size, y_size,                   
+        SDL_WINDOW_SHOWN
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window,
+        -1,                           
+        SDL_RENDERER_ACCELERATED      
+    );
+
+    int screen[x_size][y_size] = {0};
+
+    for(int i = 0; i < num_of_frames; i++)
     {
-        for (int y = 0; y < y_size; y++)
+        for (int x = 0; x < x_size; x++)
         {
-            int pixel_color = calculate_pixel_color(x, y, x_center, y_center, zoom);
-            printf("%d ", pixel_color);
+            for (int y = 0; y < y_size; y++)
+            {
+                screen[x][y] = calculate_pixel_color(x, y, x_center, y_center, zoom);
+            }
         }
-        printf("\n");
+        redraw_Screen(renderer, (int *)screen, x_size, y_size);
+    }
+}
+
+
+void redraw_Screen(SDL_Renderer *renderer, int *screen, int x_size, int y_size)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    for (size_t x = 0; x < x_size; x++)
+    {
+        for (size_t y = 0; y < y_size; y++)
+        {
+            if (!screen[x + y * x_size])
+                SDL_RenderDrawPoint(renderer, x, y);
+        }
     }
     
+    SDL_RenderPresent(renderer);
 }
+        
--- mondelbrot_arr.cpp
+++ mondelbrot_arr.cpp
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include <SDL2/SDL.h>
+
 
 const int repeats_to_set = 50;
+const int num_of_frames = 1000;
 const double max_quadr_Range[4] = {100.0, 100.0, 100.0, 100.0};
+
+const int x_size = 800;
+const int y_size = 600;
 
 void convert_arr(double dst[4], const int src[4])
 {
@@ -71,7 +77,7 @@ void set_start_coord(double *start_coord, int *coord, int coord_center, double z
     div_arr(start_coord, start_coord, zoom_arr);
 }
 
-void calculete_r(double *x_coord, double *y_coord, double *r_arr)
+void calculate_r(double *x_coord, double *y_coord, double *r_arr)
 {
    double tmp[4] = {};
    cpy_arr(tmp, x_coord);
@@ -152,7 +158,7 @@ int calculate_pixel_repeats(int *x, int *y, int x_center, int y_center, double z
     double y_new[4] = {};
 
     double r_arr[4] = {};
-    calculete_r(x_coord, y_coord, r_arr);
+    calculate_r(x_coord, y_coord, r_arr);
 
 
     int is_inside[4] = {1, 1, 1, 1};
@@ -164,7 +170,7 @@ int calculate_pixel_repeats(int *x, int *y, int x_center, int y_center, double z
         cpy_arr(x_coord, x_new);
         cpy_arr(y_coord, y_new);
 
-        calculete_r(x_coord, y_coord, r_arr);
+        calculate_r(x_coord, y_coord, r_arr);
         arr_cmp(is_inside, max_quadr_Range, r_arr);
         add_arr_i(repeats, repeats, is_inside);
     }
@@ -176,8 +182,6 @@ int calculate_pixel_repeats(int *x, int *y, int x_center, int y_center, double z
 
 int main()
 {
-    int x_size = 800;
-    int y_size = 600;
     int x_center = 0.0;
     int y_center = 0.0;
     double zoom = 20.0;
@@ -185,17 +189,60 @@ int main()
     int x[4] = {};
     int y[4] = {};
 
-    int screen[x_size * y_size] = {};
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Error SDL: %s\n", SDL_GetError());
+        return 1;
+    }
 
-    for (int x_start = 0; x_start < x_size; x_start++)
+    SDL_Window *window = SDL_CreateWindow(
+        "Mondelbrot array",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        x_size, y_size,                   
+        SDL_WINDOW_SHOWN
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window,
+        -1,                           
+        SDL_RENDERER_ACCELERATED      
+    );
+
+    int screen[x_size][y_size] = {};
+
+    for(int i = 0; i < num_of_frames; i++)
     {
-        set_arr_i(x, x_start);
-        for (int y_start = 0; y_start < y_size; y_start+=4)
+        for (int x_start = 0; x_start < x_size; x_start++)
         {
-            set_diff_arr_i(y, y_start, y_start+1, y_start+2, y_start+3);
-            calculate_pixel_repeats(x, y, x_center, y_center, zoom, repeats);
+            set_arr_i(x, x_start);
+            for (int y_start = 0; y_start < y_size; y_start+=4)
+            {
+                set_diff_arr_i(y, y_start, y_start+1, y_start+2, y_start+3);
+                calculate_pixel_repeats(x, y, x_center, y_center, zoom, repeats);
 
-            cpy_arr_i(screen + (x_start + y_start * x_size), repeats);
+                cpy_arr_i((int *)&screen[x_start][y_start], repeats);
+            }
         }
-    }    
+
+        redraw_Screen(renderer, (int *)&screen, x_size, y_size);
+    }  
+}
+
+
+void redraw_Screen(SDL_Renderer *renderer, int *screen, int x_size, int y_size)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    for (size_t x = 0; x < x_size; x++)
+    {
+        for (size_t y = 0; y < y_size; y++)
+        {
+            if (!screen[x + y * x_size])
+                SDL_RenderDrawPoint(renderer, x, y);
+        }
+    }
+    
+    SDL_RenderPresent(renderer);
 }
